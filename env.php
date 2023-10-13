72bd0876a98a2f19d7a8697dab19eb94faab8121
matheusjardim-dev <121054333+matheusjardim-dev@users.noreply.github.com>
2023-10-13 10:33:41 -0300

    Update env.php

--- env.php
+++ env.php
@@ -2,8 +2,8 @@
 
 putenv('DISPLAY_ERRORS_DETAILS=' . true);
 
-putenv('Intellifood_db_MYSQL_HOST=localhost');
-putenv('Intellifood_db_MYSQL_DBNAME=Intellifood_db');
+putenv('Intellifood_db_MYSQL_HOST=');
+putenv('Intellifood_db_MYSQL_DBNAME=');
 putenv('Intellifood_db_MYSQL_USER=root');
 putenv('Intellifood_db_MYSQL_PASSWORD=');
-putenv('Intellifood_db_MYSQL_PORT=3306');
+putenv('Intellifood_db_MYSQL_PORT=');
